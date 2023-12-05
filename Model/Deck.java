6f0f93ee2cdce8aa82bc4f5b9f415536df6b392b
jcapitanmoreno <jcapitanmoreno@gmail.com>
2023-12-05 11:12:07 +0100

    creacion de la baraja y prueba de ello

--- Model/Deck.java
+++ Model/Deck.java
@@ -1,4 +1,23 @@
 package Model;
 
+import java.util.Arrays;
+
 public class Deck {
+
+    private Card[] cards = new Card[52];
+    public void crearDeck() {
+        int inicio = 0;
+        String[] suits = {"Corazones", "Picas", "Treboles", "Diamantes"};
+        for (String suit : suits) {
+            for (int i = 1; i <= 13; i++) {
+               cards[inicio] = new Card(i, suit);
+              inicio++;
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Deck{" + "cards=" + Arrays.toString(cards) + '}';
+    }
 }
